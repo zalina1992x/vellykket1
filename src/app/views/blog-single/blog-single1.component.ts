a3ed7cb5e87d30aaa3db721b928c2caa9654736e
zalina1992x <sales@mustangbodykit.com>
2025-05-16 19:40:48 +0200

    wefeerwewe

--- src/app/views/blog-single/blog-single1.component.ts
+++ src/app/views/blog-single/blog-single1.component.ts
@@ -3,11 +3,11 @@ import { GsapRevealDirective } from '../../directives/gsap-reveal.directive';
 import { TopbarComponent } from "../../layout/components/topbar/topbar.component";
 
 @Component({
-  selector: 'app-blog-single',
+  selector: 'app-blog-single1',
   imports: [GsapRevealDirective, TopbarComponent],
   templateUrl: './blog-single1.component.html',
   styles: ``
 })
-export class BlogSingleComponent {
+export class BlogSingle1Component {
   year = new Date().getFullYear()
 }
